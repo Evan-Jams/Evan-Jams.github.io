fca18c5f19f2c1785c6751223dad0d59bcfb1519
Evan James <thevan777@gmail.com>
2020-02-08 13:50:11 -0500

    Pulled first 150 original pokemon info from pokeapi and logged them

--- pokemon_game/app.js
+++ pokemon_game/app.js
@@ -1,1 +1,20 @@
 console.log('hi');
+
+const $pokemonList = []
+for (let i = 1; i <= 150; i++){
+    const $url = `https://pokeapi.co/api/v2/pokemon/${i}`
+
+    $.ajax ({
+        url: $url,
+        type: "GET",
+
+    }).then((data) => {
+        // console.log(data);
+        const $pokemon = {
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+        }
+        console.log($pokemon);
+    })
+}
