177b921b72e233083a5f2e3f1b728e39c4707ff6
Evan James <thevan777@gmail.com>
2020-02-08 17:21:32 -0500

    Appended the classic 150 pokemon to the html

--- pokemon_game/app.js
+++ pokemon_game/app.js
@@ -1,20 +1,50 @@
 console.log('hi');
 
-const $pokemonList = []
-for (let i = 1; i <= 150; i++){
-    const $url = `https://pokeapi.co/api/v2/pokemon/${i}`
 
-    $.ajax ({
-        url: $url,
-        type: "GET",
 
-    }).then((data) => {
-        // console.log(data);
-        const $pokemon = {
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-        }
-        console.log($pokemon);
-    })
+const $pokedex = $('#pokedex')
+const pokemonList = []
+
+$(() => {
+const getPokemon = () => {
+    for (let i = 1; i <= 150; i++){
+        const $url = (`https://pokeapi.co/api/v2/pokemon/${i}`)
+
+        $.ajax ({
+            url: $url,
+            type: 'Get',
+            async: false,
+
+        }).then((data) => {
+            // console.log(data);
+            const $pokemonInfo = {
+                name: data.name,
+                id: data.id,
+                abilities: data.,
+                image: data.sprites['front_default'],
+            }
+            // console.log($pokemonInfo);
+            displayPokemon($pokemonInfo)
+            pokemonList.push($pokemonInfo);
+        })
+
+    }
+
+    // console.log(pokemonList);
+}
+getPokemon()
+
+})
+
+// console.log(pokemonList);
+
+const displayPokemon = (pokemon) => {
+    pokemon = $(`
+        <div>
+            <img src="${pokemon.image}"/>
+            <h2>${pokemon.id}. ${pokemon.name}</h2>
+            <p>Abilities: ${pokemon.abilities}</p>
+        </div>`
+    );
+    $pokedex.append(pokemon)
 }
