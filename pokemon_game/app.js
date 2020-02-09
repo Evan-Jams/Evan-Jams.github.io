c099110dc6d623c76c535c325f88de6fb911a5a2
Evan James <thevan777@gmail.com>
2020-02-09 00:33:55 -0500

    Added a carousel effect to the pokemon pokedex

--- pokemon_game/app.js
+++ pokemon_game/app.js
@@ -4,32 +4,66 @@ console.log('hi');
 
 const $pokedex = $('#pokedex')
 const pokemonList = []
+const displayPokemon = (pokemon) => {
+    pokemon = $(`
+        <div class="pokemon">
+            <img src="${pokemon.image}" alt="${pokemon.name}"/>
+            <h2>${pokemon.id}. ${pokemon.name}</h2>
+            <p>Power: 50</p>
+        </div>`
+    );
+    $pokedex.append(pokemon)
+}
 
 $(() => {
-const getPokemon = () => {
-    for (let i = 1; i <= 150; i++){
-        const $url = (`https://pokeapi.co/api/v2/pokemon/${i}`)
+    const getPokemon = () => {
+        for (let i = 1; i <= 150; i++){
+            const $url = (`https://pokeapi.co/api/v2/pokemon/${i}`)
 
-        $.ajax ({
-            url: $url,
-            type: 'Get',
-            async: false,
+            $.ajax ({
+                url: $url,
+                type: 'Get',
+                async: false,
 
-        }).then((data) => {
-            // console.log(data);
-            const $pokemonInfo = {
-                name: data.name,
-                id: data.id,
-                abilities: data.,
-                image: data.sprites['front_default'],
-            }
-            // console.log($pokemonInfo);
-            displayPokemon($pokemonInfo)
-            pokemonList.push($pokemonInfo);
-        })
+            }).then((data) => {
+                // console.log(data);
+                const $pokemon = {
+                    name: data.name,
+                    id: data.id,
+                    image: data.sprites['front_default'],
+                }
 
-    }
+                // console.log($pokemonInfo);
+                displayPokemon($pokemon)
+                pokemonList.push($pokemon);
+            })
 
+        }
+// Cycling through the pokedex functions =========================//
+        let currentImgIndex = 0
+        let maxIndex = 149
+
+    $('#next').on('click', (event) => {
+        event.preventDefault();
+        $('#pokedex').children().eq(currentImgIndex).css('display', 'none');
+        if (currentImgIndex < maxIndex) {
+            currentImgIndex++
+        } else {
+            currentImgIndex = 0
+        }
+
+        $('#pokedex').children().eq(currentImgIndex).css('display', 'block');
+    })
+    $('#previous').on('click', (event) => {
+        event.preventDefault();
+        $('#pokedex').children().eq(currentImgIndex).css('display', 'none');
+        if (currentImgIndex > 0) {
+            currentImgIndex--
+        } else {
+            currentImgIndex = 149
+        }
+        $('#pokedex').children().eq(currentImgIndex).css('display', 'block');
+    })
     // console.log(pokemonList);
 }
 getPokemon()
@@ -38,13 +72,13 @@ getPokemon()
 
 // console.log(pokemonList);
 
-const displayPokemon = (pokemon) => {
-    pokemon = $(`
-        <div>
-            <img src="${pokemon.image}"/>
-            <h2>${pokemon.id}. ${pokemon.name}</h2>
-            <p>Abilities: ${pokemon.abilities}</p>
-        </div>`
-    );
-    $pokedex.append(pokemon)
-}
+// const displayPokemon = (pokemon) => {
+//     pokemon = $(`
+//         <div class="pokemon">
+//             <img src="${pokemon.image}"/>
+//             <h2>${pokemon.id}. ${pokemon.name}</h2>
+//             <p>Power: 50</p>
+//         </div>`
+//     );
+//     $pokedex.append(pokemon)
+// }
